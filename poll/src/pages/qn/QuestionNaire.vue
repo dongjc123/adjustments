2e4a6855c57b3e9a83ab71f9f519d35470c28a89
gnddjc <gnddjc@163.com>
2018-11-06 18:23:12 +0800

    this is first commit

---
+++ poll/src/pages/qn/QuestionNaire.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="user">
+		<!-- 按钮区 -->
+		<div class="btns">
+			<el-button size='mini' type="primary" @click='toAddClazz'>添加</el-button>
+			<el-button size='mini' type="danger" @click='batchDeleteClazzes'>批量删除</el-button>
+		</div>
+		<!-- 按钮去结束 -->
+		<!-- 数据区 -->
+		
+		<!-- 数据区结束 -->
+
+	</div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<style>
+	
+</style>
